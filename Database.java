9cbf75bb41d00bae9bd9f4d70807a535efea1fbf
Manoj Dayaram <i.am.noj@gmail.com>
2012-02-29 00:46:00 -0800

    Essentials of database are built, all that's left is the parser for input.

---
+++ Database.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Database {
+
+	private final HashMap<String, Integer> db;
+	private final Stack<Rollback> rollbacks;
+	
+	public Database() {
+		this.db = new HashMap<String, Integer>();
+		this.rollbacks = new Stack<Rollback>();
+	}
+
+	public void set(String var, Integer val) {
+		Integer prev = db.get(var);
+		if (!rollbacks.isEmpty()) {
+			rollbacks.push(new Rollback(var, prev));
+		}
+		db.put(var, val);
+	}
+
+	public Integer get(String var) {
+		return db.get(var);
+	}
+
+	public void begin() {
+		rollbacks.push(null); // marks the beginning of a transaction.
+	}
+
+	public void rollback() throws InvalidRollbackException {
+		if (rollbacks.isEmpty()) {
+			throw new InvalidRollbackException();
+		}
+		while(rollbacks.peek() != null) {
+			Rollback roll = rollbacks.pop();
+			db.put(roll.getVar(), roll.getVal());
+		}
+		rollbacks.pop();
+	}
+
+	public void commit() {
+		rollbacks.clear();
+	}
+
+	public static class InvalidRollbackException extends Exception { }
+}
+
---
+++ Rollback.java
@@ -0,0 +1,15 @@
+
+public class Rollback {
+
+	private final String var;
+	private final Integer val;
+
+	public Rollback(String var, Integer val) {
+		this.var = var;
+		this.val = val;
+	}
+
+	public String getVar() { return var; }
+	public Integer getVal() { return val; }
+}
+
